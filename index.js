38fec76f503099e8d596c67705a2aa194fa05fb6
Matheus de Oliveira <85849222+Matheus21Oliveira@users.noreply.github.com>
2021-08-30 17:30:07 -0300

    add file joaninha

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js"); 
+const client = new Discord.Client(); 
+const config = require("./config.json");
+
+client.login(config.token);
+
+client.on('message', message =>{
+    if(message.author.bot) return;
+    if (message.channel.type == 'dm') return;
+    if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return;
+    if (message.content.startsWith(`<@!${client.user.id}`) || message.content.startsWith(`@${client.user.id}`)) return;
+
+    const args = message.content
+        .trin().slice(config.prefix.length)
+        .split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try {
+        const commandFile = require(`./commands/${command}.js`)
+        commandFile.run(client, message, args);
+    } catch (err) {
+        console.error('erro' + err);    
+    }
+});
+
+
+
+/*const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);*/
+
+
